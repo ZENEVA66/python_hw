6bcc7d7375a00d8b6d2f3f4e934d2ce3f2e91932
ZENEVA66 <itayzeneva1@gmail.com>
2022-07-10 18:58:58 +0300

    hm_lesson3.py

---
+++ hm_lesson3.py
@@ -0,0 +1,11 @@
+# 1. להכין רשימה של שמות 
+# 2. ליצור לולאה אשר אומרת בוקר טוב ולילה טוב לכל שם ברשימה
+# 3. קודם הוא יגיד לכולם בוקר טוב ואחר כך הוא יגיד לכולם לילה טוב 
+# Bonus - > שלב 3 לעשות בלולאה 1 
+
+mystr="itay shay raz noam"
+mylist=mystr.split()
+for i in mylist:
+    print("good morning",i,"good night")
+
+
